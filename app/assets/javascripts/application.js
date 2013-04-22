3e9eac56d5563970a9c2f1872017cc4f250deb17
Thomas Lin <ithomas.lin@me.com>
2013-04-23 00:48:06 +0100

    Config the dropdown menu

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,9 @@
 //= require jquery.masonry.min.js
 //= require bootstrap
 //= require_tree .
+
+$(function() {
+   $('#flash').delay(500).fadeIn('normal', function() {
+      $(this).delay(2500).fadeOut();
+   });
+});
