7a5dfdd275a74b6af0996eba986524af3e11e29c
Thomas Lin <ithomas.lin@me.com>
2013-05-04 18:52:19 +0100

    Auto scroll up

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,3 +21,18 @@ $(function() {
       $(this).delay(2000).toggle("slow");
    });
 });
+
+jQuery(document).ready(function(){
+    jQuery(window).scroll(function(){
+        if (jQuery(this).scrollTop() > 100) {
+            jQuery('.scrollup').fadeIn();
+        } else {
+            jQuery('.scrollup').fadeOut();
+    }
+});
+// scroll-to-top animate
+jQuery('.scrollup').click(function(){
+    jQuery("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+    });
+});
